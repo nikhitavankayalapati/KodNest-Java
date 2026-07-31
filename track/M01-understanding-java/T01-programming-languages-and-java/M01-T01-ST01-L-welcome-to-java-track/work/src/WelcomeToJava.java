3a82330b1f9285b4026ef0fc474fd93d5dd66ab3
nikhitavankayalapati <nikhitavankayalapati17@gmail.com>
2026-07-31 11:14:15 +0530

    added one more line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,10 +1,11 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       System.out.println("Welcome to Java Track");
-       System.out.println("My name is Nikhita");
-       System.out.println("My goal is to become a java developer");
-       System.out.println("i Selected java because i want to build large applications");  
-       System.out.println("I would like to work on enterprise level applications");  
-    
+        System.out.println("Welcome to Java Track");
+        System.out.println("My name is Nikhita");
+        System.out.println("My goal is to become a java developer");
+        System.out.println("i Selected java because i want to build large applications");
+        System.out.println("I would like to work on enterprise level applications");
+        System.out.println("I like problem solving ");
+
     }
 }
