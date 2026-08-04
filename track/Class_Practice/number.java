614952315459edfe88c4f25acc161a6c9cd0a940
nikhitavankayalapati <nikhitavankayalapati17@gmail.com>
2026-08-04 12:24:00 +0530

    day 3 2nd change

---
+++ track/Class_Practice/number.java
@@ -0,0 +1,15 @@
+package track.Class_Practice;
+
+public class number {
+    public static void main(String[] args) {
+        int num = 20;
+        if (num > 0) {
+            System.out.println("positive");
+        } else if (num < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+    }
+
+}
