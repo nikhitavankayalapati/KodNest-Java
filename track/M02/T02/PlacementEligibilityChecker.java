2c8a75f6659e8b58a3b3959b7fff9a93525d72e3
nikhitavankayalapati <nikhitavankayalapati17@gmail.com>
2026-08-04 13:24:31 +0530

    added placement checking file

---
+++ track/M02/T02/PlacementEligibilityChecker.java
@@ -0,0 +1,16 @@
+package track.M02.T02;
+
+public class PlacementEligibilityChecker {
+    public static void main(String[] args) {
+        int marks = 72;
+        int attendance = 80;
+        boolean projectCompleted = true;
+
+        boolean marksEligible = marks >= 60;
+        boolean attendanceEligible = attendance >= 75;
+        boolean academicEligible = marksEligible && attendanceEligible;
+        boolean placementEligible = academicEligible && projectCompleted;
+
+        System.out.println("Placement Eligible: " + placementEligible);
+    }
+}
