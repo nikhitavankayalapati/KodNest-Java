b7d951af45b9801b8b4755f77ba1f5d44efb95de
nikhitavankayalapati <nikhitavankayalapati17@gmail.com>
2026-08-07 18:34:01 +0530

    casting shift

---
+++ track/Practice_Sessions/CastingShiftInput.java
@@ -0,0 +1,21 @@
+package track.Practice_Sessions;
+
+import java.util.Scanner;
+
+public class CastingShiftInput {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        String fullName = scanner.nextLine();
+        String city = scanner.nextLine();
+
+        System.out.println("Name: " + fullName);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+
+        scanner.close();
+    }
+}
