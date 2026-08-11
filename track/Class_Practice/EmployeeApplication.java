3ce79d58880b7800f0ed1ece694fad5caf3d91fa
nikhitavankayalapati <nikhitavankayalapati17@gmail.com>
2026-08-11 11:10:00 +0530

    added method

--- track/Class_Practice/EmployeeApplication.java
+++ track/Class_Practice/EmployeeApplication.java
@@ -3,6 +3,10 @@ package track.Class_Practice;
 class Employee {
     int empid;
     String Name;
+
+    void work() {
+        System.out.println("Working");
+    }
 }
 
 public class EmployeeApplication {
@@ -12,6 +16,7 @@ public class EmployeeApplication {
         e1.Name = "nikhita";
         System.out.println(e1.empid);
         System.out.println(e1.Name);
+        e1.work();
         Employee e2 = e1;
         e2.empid = 17;
         e2.Name = "harshini";
@@ -20,5 +25,4 @@ public class EmployeeApplication {
         System.out.println(e2.empid);
         System.out.println(e2.Name);
     }
-
 }
