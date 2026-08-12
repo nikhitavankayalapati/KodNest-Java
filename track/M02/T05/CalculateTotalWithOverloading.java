1cf58be8d268728d9572a964abc7b5f7d4ddd8dd
nikhitavankayalapati <nikhitavankayalapati17@gmail.com>
2026-08-12 13:13:51 +0530

    added file

---
+++ track/M02/T05/CalculateTotalWithOverloading.java
@@ -0,0 +1,28 @@
+package track.M02.T05;
+
+import java.util.Scanner;
+
+class ResultCalculator {
+    int getTotal(int first, int second) {
+        return first + second;
+    }
+
+    int getTotal(int first, int second, int third) {
+        return first + second + third;
+    }
+}
+
+public class CalculateTotalWithOverloading {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int first = sc.nextInt();
+        int second = sc.nextInt();
+        int third = sc.nextInt();
+
+        ResultCalculator rc = new ResultCalculator();
+
+        System.out.println("Two-Mark Total: " + rc.getTotal(first, second));
+        System.out.println("Three-Mark Total: " + rc.getTotal(first, second, third));
+    }
+}
