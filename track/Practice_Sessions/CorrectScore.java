9eee94b32be3f04cebb28b2efb3e203d6b4834a2
nikhitavankayalapati <nikhitavankayalapati17@gmail.com>
2026-08-17 16:23:51 +0530

    Added file

---
+++ track/Practice_Sessions/CorrectScore.java
@@ -0,0 +1,48 @@
+package track.Practice_Sessions;
+
+import java.util.Scanner;
+
+class ScoreEditor {
+
+    void correctScore(int[] scores, int index, int newScore) {
+        if (index >= 0 && index < scores.length) {
+            scores[index] = newScore;
+        } else {
+            System.out.println("Invalid index");
+        }
+    }
+
+    void displayScores(int[] scores) {
+        System.out.print("Scores: ");
+        for (int score : scores) {
+            System.out.print(score + " ");
+        }
+        System.out.println();
+    }
+}
+
+public class CorrectScore {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+        int[] scores = new int[size];
+
+        // Read all scores
+        for (int index = 0; index < scores.length; index++) {
+            scores[index] = scanner.nextInt();
+        }
+
+        // Read correction details
+        int correctionIndex = scanner.nextInt();
+        int newScore = scanner.nextInt();
+
+        // Correct and display
+        ScoreEditor se = new ScoreEditor();
+        se.correctScore(scores, correctionIndex, newScore);
+        se.displayScores(scores);
+
+        scanner.close();
+    }
+}
