1775c54e94acf0661314b4c2abaab17323463952
nikhitavankayalapati <nikhitavankayalapati17@gmail.com>
2026-09-10 17:38:06 +0530

    stringBuilder files added

--- track/M04/T02/ReverseAString.java
+++ track/M04/T02/ReverseAString.java
@@ -11,3 +11,33 @@ public class ReverseAString {
         scanner.close();
     }
 }
+
+/*
+ * import java.util.Scanner;
+ * 
+ * public class RevString {
+ * public static void main(String[] args) {
+ * Scanner scan = new Scanner(System.in);
+ * System.out.print("Enter the String: ");
+ * String str = scan.next();
+ * 
+ * char arr[] = str.toCharArray();
+ * char newArr[] = new char[arr.length];
+ * 
+ * // Start 'j' at the last valid index of the new array
+ * int j = newArr.length - 1;
+ * 
+ * // Fix the loop condition to stop before 'arr.length'
+ * for (int i = 0; i < arr.length; i++) {
+ * newArr[j] = arr[i];
+ * j--;
+ * }
+ * 
+ * String revStr = new String(newArr);
+ * System.out.println("Original String: " + str);
+ * System.out.println("Reversed String: " + revStr);
+ * 
+ * scan.close();
+ * }
+ * }
+ */
